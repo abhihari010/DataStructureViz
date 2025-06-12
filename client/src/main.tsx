efe39b7b46c5f2fa4519785ef3bdb5e0444b62e8
abhihari010 <abhihari010@vt.edu>
2025-06-12 18:42:45 -0400

    minor issue fix with main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { Provider } from "@radix-ui/react-toast";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<Provider><App /></Provider>);
