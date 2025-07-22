602d03aac6d399700bf14b79744c2cab8f72dd93
abhihari010 <abhihari010@gmail.com>
2025-07-22 11:47:09 -0400

    redirects added for auth pages to prevent 404 errors during auth transitions.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Redirect } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -68,6 +68,16 @@ function Router() {
           <Route path="/dfs" component={DFS} />
           <Route path="/bfs" component={BFS} />
           <Route path="/dijkstra" component={Dijkstra} />
+          {/* Redirect authenticated users trying to access public routes */}
+          <Route path="/login" component={() => <Redirect to="/" />} />
+          <Route path="/register" component={() => <Redirect to="/" />} />
+          <Route path="/verify-email" component={() => <Redirect to="/" />} />
+          <Route
+            path="/forgot-password"
+            component={() => <Redirect to="/" />}
+          />
+          <Route path="/reset-password" component={() => <Redirect to="/" />} />
+          <Route path="/verify-otp" component={() => <Redirect to="/" />} />
         </>
       )}
       <Route component={NotFound} />
