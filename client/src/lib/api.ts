ebfa0774c921db7e8d7c263a1fc591953a4b43b4
abhihari010 <abhihari010@gmail.com>
2025-08-09 22:00:24 -0400

    more changes forr backend and client

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL ||
-    "https://datastructurevizbackend.onrender.com/api",
+    "https://datastructurevizbackend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
