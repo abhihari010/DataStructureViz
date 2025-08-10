57759d4eb8e9f2b6f0d52341838e3dbcd78baa3d
abhihari010 <abhihari010@gmail.com>
2025-08-09 22:13:50 -0400

    base url changes made again

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL ||
-    "https://datastructurevizbackend.onrender.com",
+    "https://datastructurevizbackend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
